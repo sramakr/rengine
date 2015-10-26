729a3aa9d92900bfd0331b95e633ca393bb74fb8
sramakr <sramakr@gmail.com>
2015-10-26 19:27:15 +0000

    added jmx integration

--- rule.py
+++ rule.py
@@ -1,26 +1,43 @@
 import yaml,json,requests,logging
 from brain import Brain
+import dataset
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+from jvm import JAVA
+
 logging.basicConfig()
 logger = logging.getLogger()
 logger.getChild("brain").setLevel(logging.INFO)
 logger.setLevel(logging.DEBUG)
+sched = BlockingScheduler()
+db = dataset.connect('sqlite:///:memory:')
 
 with open("conf/rule2.yaml", 'r') as stream:
     config=yaml.load(stream)
 
 
-def action(config):
+def action():
+	print "hello world"
 	actions=config.get('then').get('actions')
 	for action in actions:
 		brain=Brain(action.get('args'))
 		brain.action(action.get('name'))
 
-def input(config):
+@sched.scheduled_job('interval', seconds=10)
+def input():
+	actionflg=True
 	inputs=config.get('if').keys()
 	for input in inputs:
-		print input
+		logger.debug(input)
 		brain=Brain(config)
 		brain.action(input)
+		if not brain.outflag:
+			actionflg=False
+			break
+	if actionflg:
+		action()
 
-if not input(config):
-	action(config)
+#Initialize JVM if jmx metrics are being checked
+if 'jmx' in config.get('if').keys():
+	JAVA()
+sched.start()
